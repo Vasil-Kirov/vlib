6a1cf75df1655125b750d9e257ca3121cbc15a4f
Vasil-Kirov <vaskoabs@gmail.com>
2024-07-05 12:14:44 +0200

    made strings heap allocated... eh

--- vstring.h
+++ vstring.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <memory.h>
 
+void *VLibMemDup(void *, size_t);
+
 typedef struct String {
 	const char *data;
 	size_t len;
@@ -19,23 +21,39 @@ typedef struct String {
 #endif
 } String;
 
-#define STR_LIT(STR) make_string(STR, sizeof(STR) - 1)
+#define STR_LIT(STR) VLibMakeString(STR, sizeof(STR) - 1)
 
 String VLibMakeString(const char *str, size_t len);
+void VLibDeleteString(String *str);
 
 #if !defined(VLIB_NO_SHORT_NAMES)
 #define MakeString VLibMakeString
+#define MemDup VLibMemDup
 #endif
 
 
 #if defined(VLIB_IMPLEMENTATION)
 
+void *MemDup(void *mem, size_t size)
+{
+	void *result = malloc(size);
+	memcpy(result, mem, size);
+	return result;
+}
+
 String VLibMakeString(const char *str, size_t len)
 {
 	String result;
-	result.data = str;
+	result.data = (const char *)VLibMemDup((void *)str, len);
 	result.len = len;
 	return result;
+}
+
+void VLibDeleteString(String *str)
+{
+	free((void *)str->data);
+	str->data = NULL;
+	str->len = 0;
 }
 
 #endif
